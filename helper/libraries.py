075740a7e541231e70b058dfa6b2ec1aacb0b129
Mayank Parihar <m.parihar@somaiya.edu>
2022-12-06 19:01:10 +0530

    Deploy to Streamlit Cloud

--- helper/libraries.py
+++ helper/libraries.py
@@ -4,7 +4,7 @@ import streamlit as st
 from keras import layers, models, optimizers  # modeling
 import json
 from keras.models import load_model
-import opencv-python
+#import opencv-python
 import cv2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
